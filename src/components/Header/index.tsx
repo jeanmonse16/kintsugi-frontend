f73d04696f8e835b5ca01b45321dfa4c5c6c4192
JoalbertMilano <joalbertmilano@gmail.com>
2022-04-23 18:11:34 -0400

    Adding particules and improving Hero

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,11 @@
 import Logo  from '../../assets/images/logo.jpeg'
+import Letter from '../../assets/images/logo-letters.png'
 
 export const Header = () => {
     return (
         <header className="p-4 h-[5.5rem] flex items-center">
             <img className="w-14 rounded-full" src={Logo} alt="Kintsugi logo" />
-            <span className="ml-4 text-white text-xl font-semibold">Kintsugi NFTs</span>
+            <img className="ml-4 w-32" src={Letter} alt="Kintsugi logo letters" />
         </header>       
     )
 }
--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -1,13 +1,149 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 import { Header } from "../components/Header"
 import { Hero } from "../components/Hero"
 import { Footer } from "../components/Footer"
 
+// export const ComingSoon = () => {
+//     return (
+//         <div className="h-screen grid grid-rows-[min-content_1fr_min-content] gap-4">
+//             <Header />
+//             <Hero />
+//             <Footer />
+//         </div>
+//     )
+// }
+
 export const ComingSoon = () => {
+    const options = {
+        fpsLimit: 120,
+        particles: {
+          number: {
+            value: 200,
+            density: {
+              enable: true,
+              value_area: 800
+            }
+          },
+          color: {
+            value: "#fc07fa"
+          },
+          shape: {
+            type: "circle",
+            stroke: {
+              width: 0,
+              color: "#000000"
+            },
+            polygon: {
+              nb_sides: 5
+            },
+            image: {
+              src: "img/github.svg",
+              width: 100,
+              height: 100
+            }
+          },
+          opacity: {
+            value: 1,
+            random: true,
+            anim: {
+              enable: true,
+              speed: 1,
+              opacity_min: 0,
+              sync: false
+            }
+          },
+          size: {
+            value: 3,
+            random: true,
+            anim: {
+              enable: false,
+              speed: 4,
+              size_min: 0.3,
+              sync: false
+            }
+          },
+          line_linked: {
+            enable: false,
+            distance: 150,
+            color: "#ffffff",
+            opacity: 0.4,
+            width: 1
+          },
+          move: {
+            enable: true,
+            speed: 1,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "bounce",
+            bounce: false,
+            attract: {
+              enable: false,
+              rotateX: 600,
+              rotateY: 600
+            }
+          }
+        },
+        interactivity: {
+          detect_on: "canvas",
+          events: {
+            onhover: {
+              enable: false,
+              mode: "grab"
+            },
+            onclick: {
+              enable: false,
+              mode: "repulse"
+            },
+            resize: true
+          },
+          modes: {
+            grab: {
+              distance: 400,
+              line_linked: {
+                opacity: 1
+              }
+            },
+            bubble: {
+              distance: 250,
+              size: 0,
+              duration: 2,
+              opacity: 0,
+              speed: 3
+            },
+            repulse: {
+              distance: 400,
+              duration: 0.4
+            },
+            push: {
+              particles_nb: 4
+            },
+            remove: {
+              particles_nb: 2
+            }
+          }
+        },
+        retina_detect: true
+    }
+
+    const particlesInit = async (main) => {
+      await loadFull(main);
+    };
+  
     return (
-        <div className="h-screen grid grid-rows-[min-content_1fr_min-content] gap-4">
-            <Header />
-            <Hero />
-            <Footer />
-        </div>
-    )
-}
+        <>
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                options={options}
+            />
+            <div className="relative h-screen grid grid-rows-[min-content_1fr_min-content] gap-4 z-30">
+                    <Header />
+                    <Hero />
+                    <Footer />
+            </div>
+        </>
+        
+    );
+  };
