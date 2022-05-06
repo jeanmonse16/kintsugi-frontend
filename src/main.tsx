2a4d7392bedd680672eae9987e899075a345e77a
jeanpier_monserrate <jeanpier.dev@gmail.com>
2022-05-06 12:57:26 -0400

    customizing build

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOMClient from 'react-dom/client'
 import { App } from './App'
 import './assets/styles/tailwind.css'
 
-console.log(import.meta.env.VITE_MESSAGE)
-
 const container = document.getElementById('root')
 const root = ReactDOMClient.createRoot(container)
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-  ]
+    build: {
+        rollupOptions: {
+            output: {
+                dir: './dist',
+                assetFileNames: 'assets/[name][extname]',
+                chunkFileNames: 'assets/[name].js',
+                entryFileNames: 'assets/[name].js'
+            }
+        }
+    },
+    plugins: [
+        react(), 
+    ]
 })
