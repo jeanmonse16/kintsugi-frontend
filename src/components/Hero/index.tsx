1707f0f4728e8fd8bc23d80cc10676df8464ec5d
jeanpier_monserrate <jeanpier.dev@gmail.com>
2022-05-07 00:40:40 -0400

    adding assets env variable

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,9 +1,7 @@
-import Coming from '../../assets/images/coming-soon-yellow.png'
-
 export const Hero = () => {
     return (
         <main className="px-4 place-self-center">
-            <img className="w-96" src={Coming} alt="Coming soon" />
+            <img className="w-96" src={__ASSETS_URL__ + 'coming-soon-yellow.png'} alt="Coming soon" />
         </main>
     )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    build: {
-        rollupOptions: {
-            output: {
-                dir: './dist',
-                assetFileNames: 'assets/[name][extname]',
-                chunkFileNames: 'assets/[name].js',
-                entryFileNames: 'assets/[name].js'
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(),'')
+    return {
+        define: {
+            __ASSETS_URL__: JSON.stringify(env.VITE_ASSETS_URL)
+        },
+        build: {
+            rollupOptions: {
+                output: {
+                    dir: './dist',
+                    assetFileNames: 'assets/[name][extname]',
+                    chunkFileNames: 'assets/[name].js',
+                    entryFileNames: 'assets/[name].js'
+                }
             }
-        }
-    },
-    plugins: [
-        react(), 
-    ]
+        },
+        plugins: [
+            react(), 
+        ]
+    }
 })
